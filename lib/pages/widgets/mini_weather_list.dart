cfba3802f939f171f6e0cbd9dfac03c70de449ca
Fajar Riansyah <fajary781@gmail.com>
2025-08-14 11:30:26 +0700

    feat: add mini weather list widget

---
+++ lib/pages/widgets/mini_weather_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MiniWeatherList extends StatelessWidget {
+  const MiniWeatherList({
+    super.key,
+    required this.future,
+    required this.child,
+  });
+
+  final Future future;
+  final Widget Function(AsyncSnapshot<dynamic> snapshot) child;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator.adaptive(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error : ${snapshot.error}'),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data == null) {
+          return const Center(
+            child: Text('No data available'),
+          );
+        }
+
+        return child(snapshot);
+      },
+    );
+  }
+}
