29667eb4f078f2842288ef13503ec30517a8a1de
Fajar Riansyah <fajary781@gmail.com>
2025-08-14 23:42:42 +0700

    refactor (main): update remote data sources to use concrete class

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 import 'data/remote_data_sources.dart';
-import 'data/remote_data_sources_mock.dart';
+import 'data/remote_data_sources_impl.dart';
 import 'pages/home_page.dart';
 import 'repositories/repositories.dart';
 import 'repositories/repositories_impl.dart';
 
-late final RemoteDataSources<String> remoteDataSources;
+late final RemoteDataSources<Response> remoteDataSources;
 late final Repositories repositories;
 
 void main() {
-  remoteDataSources = RemoteDataSourcesMock();
+  // remoteDataSources = RemoteDataSourcesMock();
+  remoteDataSources = RemoteDataSourcesImpl(httpClient: Client());
   repositories = RepositoriesImpl(remoteDataSources: remoteDataSources);
   runApp(const MyApp());
 }
